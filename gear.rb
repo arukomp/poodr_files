efaf7ce6ec1889399ca9f4e216a63ad034ec5390
Arunas Skirius <arukomp@gmail.com>
2016-09-27 11:16:27 +0100

    Ch3, isolate multiparameter initialization

--- gear.rb
+++ gear.rb
@@ -1,22 +1,27 @@
-class Gear
-  attr_reader :chainring, :cog, :wheel
-  def initialize(args)
-    args = defaults.merge(args)
-    @chainring = args[:chainring]
-    @cog = args[:cog]
-    @wheel = args[:wheel]
-  end
+module SomeFramework
+  class Gear
+    attr_reader :chainring, :cog, :wheel
+    def initialize(chainring, cog, wheel)
+      @chainring = chainring
+      @cog = cog
+      @wheel = wheel
+    end
 
-  def ratio
-    chainring / cog.to_f
-  end
+    def ratio
+      chainring / cog.to_f
+    end
 
-  def gear_inches
-    ratio * wheel.diameter
+    def gear_inches
+      ratio * wheel.diameter
+    end
   end
+end
 
-  def defaults
-    {chainring: 40, cog: 18}
+module GearWrapper
+  def self.gear(args)
+    SomeFramework::Gear.new(args[:chainring],
+                           args[:cog],
+                           args[:wheel])
   end
 end
 
