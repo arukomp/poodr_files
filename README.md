f943cb36168e9f7edfb57a9363c15517957f88e5
Arunas Skirius <arukomp@gmail.com>
2016-09-27 11:13:25 +0100

    Ch3, explicitly define defaults

---
+++ README.md
@@ -0,0 +1,3 @@
+# POODR book code as I read
+
+Every code change, every experiment committed. Started at about Chapter 3.
---
+++ gear.rb
@@ -0,0 +1,37 @@
+class Gear
+  attr_reader :chainring, :cog, :wheel
+  def initialize(args)
+    args = defaults.merge(args)
+    @chainring = args[:chainring]
+    @cog = args[:cog]
+    @wheel = args[:wheel]
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+
+  def gear_inches
+    ratio * wheel.diameter
+  end
+
+  def defaults
+    {chainring: 40, cog: 18}
+  end
+end
+
+class Wheel
+  attr_reader :rim, :tire
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
+  end
+end
